0784aae3ac709e88c247768354612856a3b48c34
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2020-02-15 13:07:37 +0530

    express web server

--- web-server/index.js
+++ web-server/index.js
@@ -1,12 +1,24 @@
 const path = require('path');
-
+const hbs = require('hbs');
 const express = require('express');
+
 const app = express();
+//const port = process.env.PORT || 3000;
 
+// Define path for express config
 const publicDirectoryPath = path.join(__dirname,'/public');
+const viewsPath = path.join(__dirname,'/templates/views');
+const partialsPath = path.join(__dirname,'/templates/partials');
 
+// Setup handlebars engine.
 app.set('view engine', 'hbs'); // for dynamic content
-app.use(express.static(publicDirectoryPath)); // for static content
+// handlebars bydefault will take 'views' folder. If we want to change the loc then we have to set it
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
+// For static content
+app.use(express.static(publicDirectoryPath)); 
 
 // for dynamic 
 app.get('', (req, res) => {
@@ -32,6 +44,20 @@ app.get('/help', (req, res) => {
 // app.com/about
 app.get('/about', (req, res) => {
     res.send('Hello Saurabh. This is about.');
+});
+
+// products
+app.get('/products', (req, res) => {
+    console.log('req.query:', req.query);
+    if(!req.query.search) {
+        return res.send({
+            error: "Enter the search criteria"
+        });
+    } else {
+        res.send({
+            products : []
+        });
+    }
 });
 
 app.listen(3000, () => {
