d4e8c26ed28b42c5e16f04fee566d05740aa02fa
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2020-02-15 13:14:50 +0530

    socket-app

---
+++ socket-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DocumentComponent } from './document/document.component';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
