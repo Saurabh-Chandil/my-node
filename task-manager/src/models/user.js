30eee9960f4017e3f699263fbdb0490f23d93404
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2020-02-22 01:37:34 +0530

    pre hook for secure passwords

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = mongoose.Schema({
     name: {
         type: String
     },
@@ -17,7 +18,24 @@ const User = mongoose.model('User', {
     age: {
         type: Number,
         default: 0
+    },
+    password: {
+        type: String
     }
 });
+
+userSchema.pre('save', async function(next) {
+    // 'this' refers to doc being saved
+    const user = this;
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next(); // to transfer control to other middleware in the middleware stack. If we do not put this 
+    //then our compiler will assume that we are still running this function and user doc will not be 'save'.
+})
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
